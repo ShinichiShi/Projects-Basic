c33c1fe9e2e3cfd40a456b4146ea391bec104c2d
ShinichiShi <supreeth2020@gmail.com>
2024-05-25 21:50:48 +0530

    update changes

--- Library/script.js
+++ Library/script.js
@@ -1,40 +1,63 @@
-const submitbtn=document.querySelector('#close');
-const plus=document.querySelector('#plus');
+const submitbtn = document.querySelector('#close');
+const plus = document.querySelector('#plus');
 const details = document.querySelector('.section');
-
+const card = document.querySelector('.hero');
 // document.addEventListener('DOMContentLoaded', start);
+
+const Library = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
 start();
-function start(){
+function start() {
     plus.addEventListener('click', () => {
         details.hidden = false;
     }
     );
-}
-function end(){
     submitbtn.addEventListener('click', () => {
-        details.classList.remove('.section');
-    }
-    );
-}
-const Library = [
-    {
-    title:"title",
-    author:"author",
-    pages:"pages",
-    read:true
-    },
-    {
-        title:"title2",
-    author:"author2",
-    pages:"pages2",
-    read:true
-    }
-];
+        addbook();
+    });
 
-function Book(title,author,pages,read){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.read=read;
 }
 
+function addbook() {
+    let title = document.querySelector('#title').value;
+    let author = document.querySelector('#author').value;
+    let pages = document.querySelector('#pages').value;
+    let read = document.querySelector('#read').checked;
+    if (validate(title.trim(), author.trim(), pages.trim(), read)) {
+        let book = new Book(title, author, pages, read);
+        Library.push(book);
+        // console.log(Library);
+        display();
+        details.hidden = true;
+    }
+    else {
+
+        alert('Please enter Valid info');
+    }
+}
+
+function display() {
+    for (const index of Library) {
+        let div = document.createElement('div');
+        div.classList.add('card')
+        div.innerHTML = `
+        <div class="details">
+            <div class="title">${index.title}</div>
+            <div class="author">${index.author}</div>
+            <div class="pages">${index.pages}</div>
+            <button id="read">${index.read}</button>
+        </div> `
+        card.appendChild(div);
+    }
+}
+
+function validate(title, author, pages, read) {
+    if (title === '' || author === '' || isNaN(pages) || parseInt(pages) <= 0) { return false; }
+    return true;
+}
